0235e7587b0e45f348745817fd511b17d914ec32
RobotModel67 <robotmodel67@gmail.com>
2020-03-20 20:55:51 -0500

    Stage04 App01 with Sequelize

--- stage04/app-01.js
+++ stage04/app-01.js
@@ -1,12 +1,43 @@
 /*
     app-01.js
-    Server 1: Everything in a file. Require hapi
+    Server 1: Everything in a file. Require hapi, sequelizer, mysql2
     Test: http://localhost:3000
 */
 
 "use strict";
 
 const Hapi = require('@hapi/hapi');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('alicia', 'root', null,
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+
+const Facultad = sequelize.define('facultad',
+  {
+    facultad_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoincrement: true
+    },
+    facultad: {
+      type: Sequelize.STRING,
+    },
+    nombre_decano: {
+      type: Sequelize.STRING,
+    },
+    documento_decano: {
+      type: Sequelize.STRING,
+    },
+    firma: {
+      type: Sequelize.STRING,
+    }
+  },
+  {
+    tableName: 'facultad',
+    timestamps: false,
+  })
 
 const init = async () => {
   const server = Hapi.server({
@@ -16,8 +47,10 @@ const init = async () => {
   server.route({
     method: "GET",
     path: "/",
-    handler: (request, h) => {
-      return { message: "Hello World!" };
+    handler: async (request, h) => {
+      //return { message: "Hello World!" };
+      const result = await Facultad.findAll({}).catch(console.error());
+      return result;
     }
   });
   await server.start();
